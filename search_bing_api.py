6a9b3c30032372a5256cce1bd9302d1b87f6d6f2
cuongtv89ks <vietcuong89ks@gmail.com>
2018-11-12 22:28:35 +0900

    initializing deep learning image dataset

--- search_bing_api.py
+++ search_bing_api.py
@@ -6,4 +6,28 @@ import os
 
 # construct the argument parser nd parser argument
 ap = argparse.ArgumentParser()
+
 ap.add_argument("-q", "--query", required=True, help="search query to search Bing Image API for")
+ap.add_argument("-o", "--output", required=True, help="path to output directory of images")
+
+args = vars(ap.parse_args())
+
+# set your Microsoft Cognitive Services API key along with (1) the
+# maximum number of results for a given search and (2) the group size
+# for results (maximum of 50 per request)
+API_KEY = "3fdbf3a12fb747c8984305009ec0db05"
+MAX_RESULTS = 250
+GROUP_SIZE = 50
+
+# set the endpoint API URL
+URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
+
+# when attempting to download images from the web both the Python
+# programming language and the requests library have a number of
+# exceptions that can be thrown so let's build a list of them now
+# so we can filter on them
+EXCEPTIONS = set([IOError, FileNotFoundError,
+                  exceptions.RequestException,
+                  exceptions.HTTPError,
+                  exceptions.ConnectionError,
+                  exceptions.Timeout])
