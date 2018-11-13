295ce756d191fde07555aa1026daff52487cbb5b
cuongtv89ks <vietcuong89ks@gmail.com>
2018-11-13 16:18:51 +0900

    constructed network architecture

--- search_bing_api.py
+++ search_bing_api.py
@@ -4,6 +4,7 @@ import requests
 import cv2
 import os
 
+
 # construct the argument parser nd parser argument
 ap = argparse.ArgumentParser()
 
@@ -26,8 +27,74 @@ URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
 # programming language and the requests library have a number of
 # exceptions that can be thrown so let's build a list of them now
 # so we can filter on them
-EXCEPTIONS = set([IOError, FileNotFoundError,
-                  exceptions.RequestException,
-                  exceptions.HTTPError,
-                  exceptions.ConnectionError,
-                  exceptions.Timeout])
+EXCEPTIONS = set([IOError, OSError, exceptions.RequestException, exceptions.HTTPError, exceptions.ConnectionError,
+                 exceptions.Timeout])
+
+# store the search term in a convenience variable then set the
+# headers and search parameters
+term = args["query"]
+headers = {"Ocp-Apim-Subscription-Key" : API_KEY}
+params = {"q" : term, "offset" : 0, "count" : GROUP_SIZE}
+
+# make the search
+print("[INFO] searching Bing API for '{}' ".format(term))
+search = requests.get(URL, headers=headers, params=params)
+search.raise_for_status()
+
+# grab the results from the search, including the total number of
+# estimated results returned by the Bing API
+results = search.json()
+estNumResults = min(results["totalEstimatedMatches"], MAX_RESULTS)
+print("[INFO] {} total results for '{}' ".format(estNumResults, term))
+
+#initialize the total number of images downloaded thus far
+total = 0
+
+for offset in range(0, estNumResults, GROUP_SIZE):
+    print("[INFO] making request for group {} - {} of {}...".format(offset, offset+GROUP_SIZE, estNumResults))
+    params["offset"] = offset
+    search = requests.get(URL, headers=headers, params=params)
+    search.raise_for_status()
+    results = search.json()
+    print("[INFO] saving images for group {} - {} of {}...".format(offset, offset+GROUP_SIZE, estNumResults))
+
+    # loop for results
+    for v in results["value"]:
+        # try to download image
+        p = ""
+        try:
+            # make a request to download the image
+            print("[INFO] fetching: {}".format(v["contentUrl"]))
+            r = requests.get(v["contentUrl"], timeout=30)
+
+            # build the path to the output image
+            ext = v["contentUrl"][v["contentUrl"].rfind("."):]
+            p = os.path.sep.join([args["output"], "{}{}".format(str(total).zfill(8), ext)])
+
+            # write the image to disk
+            f = open(p, "wb")
+            f.write(r.content)
+            f.close()
+
+            # catch any errors that would not unable us to download the image
+        except Exception as e:
+            if type(e) in EXCEPTIONS:
+                print("[INFO] skipping: {}".format(v["contentUrl"]))
+                continue
+
+        # try to load the image from disk
+
+        image = cv2.imread(p)
+
+        # if the image is "None" then we could not properly load the
+        # image from disk (so it should be ignored)
+        if image is None:
+            print("[INFO] deleting: {}".format(p))
+            os.remove(p)
+            continue
+
+        # update the counter
+        total += 1
+
+
+
